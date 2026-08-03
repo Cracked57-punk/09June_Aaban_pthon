59d4e80d4cdf8cc09b49acfc6a9e8bfe02906ccc
Cracked57-punk! <aabanat2702@gmail.com>
2026-08-03 07:08:36 +0530

    Completed Task 4 of session 8

--- Assisgnments/Module 3/Session 8/Task 4/main.py
+++ Assisgnments/Module 3/Session 8/Task 4/main.py
@@ -1,1 +1,16 @@
 from insta_utils import like_count, comment_count
+
+current_likes=227
+current_comments=140
+
+print(f"Your current likes:{current_likes}\nYour  current comments:{current_comments}")
+
+current_likes =like_count(current_likes,int(input("Enter your new likes:")))
+current_comments =comment_count(current_comments,int(input("Enter your new comments:")))
+
+print(f"After update 1 -> Likes: {current_likes}, Comments: {current_comments}")
+
+current_likes = like_count(current_likes, 25)
+current_comments = comment_count(current_comments, 7)
+
+print(f"After update 2 -> Likes: {current_likes}, Comments: {current_comments}")
